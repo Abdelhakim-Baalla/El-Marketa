0c975156aa9b021ff851ba80fda7a284afacbd7e
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2026-01-04 04:40:53 +0100

    Fixer les erreurs dans la configuration du prisma client

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,17 +1,23 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  // Connexion à la base de données au démarrage du module
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL as string,
+    });
+    super({ adapter });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
 
-  // Déconnexion propre lors de l'arrêt de l'application
   async onModuleDestroy() {
     await this.$disconnect();
   }
