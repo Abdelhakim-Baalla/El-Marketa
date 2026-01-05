cf04736a7015d1c1c5177bacf810c5c9f4fe9581
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2026-01-05 01:23:11 +0100

    Finalisation de moudule & controller de l'authentification

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: config.get<number>('JWT_EXPIRES_IN') || 604800,
+        },
+      }),
+    }),
+  ],
+  controllers: [AuthController],
   providers: [AuthService],
-  controllers: [AuthController]
+  exports: [AuthService, JwtModule],
 })
 export class AuthModule {}
