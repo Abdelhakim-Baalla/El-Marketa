8239c14f12a2f3c659d02b95fd57b67b4e528978
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2026-01-08 01:04:52 +0100

    Implementation de controller des cxatalogs qui est responsable de difinir les routes et aider le front a communiquer avec notre API

--- src/catalog/catalog.controller.ts
+++ src/catalog/catalog.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
+import { CatalogService } from './catalog.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { FilterProductDto } from './dto/filter-product.dto';
 
 @Controller('catalog')
-export class CatalogController {}
+export class CatalogController {
+  constructor(private catalogService: CatalogService) {}
+
+  @Post()
+  create(@Body() createProductDto: CreateProductDto) {
+    return this.catalogService.create(createProductDto);
+  }
+
+  @Get()
+  findAll(@Query() filterDto: FilterProductDto) {
+    return this.catalogService.findAll(filterDto);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.catalogService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
+    return this.catalogService.update(id, updateProductDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.catalogService.remove(id);
+  }
+}
