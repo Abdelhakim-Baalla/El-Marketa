b1b0555f44f1352b98de4b53063f99192ceabe3f
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2026-01-12 10:39:54 +0100

    Exposer les endpoints pour gérer les commandes.

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Patch } from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { CreateOrderDto } from './dto/create-order.dto';
 
 @Controller('orders')
-export class OrdersController {}
+export class OrdersController {
+  constructor(private ordersService: OrdersService) {}
+
+  @Post()
+  create(@Body() createOrderDto: CreateOrderDto) {
+    const userId = 'temp-user-id';
+    return this.ordersService.create(userId, createOrderDto);
+  }
+
+  @Get()
+  findAll() {
+    const userId = 'temp-user-id';
+    const isAdmin = false;
+    return this.ordersService.findAll(userId, isAdmin);
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    const userId = 'temp-user-id';
+    const isAdmin = false;
+    return this.ordersService.findOne(id, userId, isAdmin);
+  }
+
+  @Patch(':id/cancel')
+  cancel(@Param('id') id: string) {
+    const userId = 'temp-user-id';
+    const isAdmin = false;
+    return this.ordersService.cancel(id, userId, isAdmin);
+  }
+}
