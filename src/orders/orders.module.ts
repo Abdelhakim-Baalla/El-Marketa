3f2a069b20c65a7354e3c3a002bd4ed37feb9325
Abdelhakim Baalla <abdelhakimbaalla50@gmail.com>
2026-01-12 10:43:31 +0100

    configurer le module Orders

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
+import { InventoryModule } from '../inventory/inventory.module';
 
 @Module({
+  imports: [InventoryModule],
+  controllers: [OrdersController],
   providers: [OrdersService],
-  controllers: [OrdersController]
 })
 export class OrdersModule {}
